d4388b83849f0d7b52209474e652339092ff9d40
david <davidf1212@gmail.com>
2015-05-13 16:19:00 -0400

    paged text

--- main.lua
+++ main.lua
@@ -2,13 +2,17 @@ Textbox = require 'textbox'
 
 function love.load()
 	tb = Textbox:new()
-	-- tb:setText('11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111')
-	tb:setText('As you probably know by now,` LOVE is a framework for making 2D games in the Lua programming language.` LOVE is totally free,` and can be used in anything from friendly open-source hobby projects,` to evil,` closed-source commercial ones.` This is the full source for \'hello world\' in LOVE.` Running this code will cause an 800 by 600 window to appear,` and display white text on a black background.`')
-
-	local cb = function()
-	end
-
-	tb:setCallback(cb)
+	-- tb:setFont(love.graphics.newFont('data/Retro Computer_DEMO.ttf', 14))
+	tb:setFont(love.graphics.newFont('data/pixelmix.ttf', 16))
+	tb:setPosition(20, 20)
+	tb:setTextSound(love.audio.newSource('aud/blip.wav'))
+	tb:setEndSound(love.audio.newSource('aud/bleep.wav'))
+	tb:setTexts({
+			'As you probably know by now,` LOVE is a framework for making 2D games in the Lua programming language.',
+			'LOVE is totally free,` and can be used in anything from friendly open-source hobby projects,` to evil,` closed-source commercial ones.',
+			'This is the full source for \'hello world\' in LOVE.',
+			'Running this code will cause an 800 by 600 window to appear,` and display white text on a black background.'
+		})
 end
 
 function love.update(dt)
--- textbox.lua
+++ textbox.lua
@@ -3,25 +3,61 @@ textbox.__index = textbox
 
 local function new()
 	local t = {}
-	t.font = love.graphics.newFont('data/Minecraftia-Regular.ttf', 16)
-	-- t.font = love.graphics.newFont(12)
-	t.textSpeed = 0.008
+	t.texts = {}
+	t.textPage = 1
+	t.font = love.graphics.newFont()
+	t.textSpeed = 0.01
 	t.beatSpeed = 0.2
 	t.timer = 0
 	t.index = 0
 	t.lines = 1
 	t.displayText = ''
-	t.callback = nil
 	t.maxWidth = 400
 	t.maxLines = 3
-	t.marginX = 20
-	t.marginY = 20
-	t.sound = love.audio.newSource('aud/blip.wav')
+	t.paddingX = 20
+	t.paddingY = 20
+	t.stopped = false
+	t.visible = true
+	t.x = 0
+	t.y = 0
 	return setmetatable(t, textbox)
 end
 
-function textbox:setCallback(callback)
-	self.callback = callback
+function textbox:setPosition(x, y)
+	self.x, self.y = x, y
+end
+
+function textbox:setTextSound(sound)
+	self.textSound = sound
+end
+
+function textbox:setEndSound(sound)
+	self.endSound = sound
+end
+
+function textbox:setFont(font)
+	self.font = font
+end
+
+function textbox:setTexts(texts)
+	self.texts = texts
+	self.textPage = 1
+	self:setText(texts[1])
+end
+
+function textbox:nextPage()
+	if #self.texts > 0 then
+		self.textPage = self.textPage + 1
+		if self.textPage <= #self.texts then
+			self:setText(self.texts[self.textPage])
+			self.stopped = false
+			self.timer = self.beatSpeed
+		else
+			self.visible = false
+		end
+	else
+		self.visible = false
+	end
 end
 
 function textbox:setText(text)
@@ -45,7 +81,7 @@ function textbox:setText(text)
 		word = word .. char
 		wordWidth = self.font:getWidth(word)
 		
-		if char == ' ' or i == #text then
+		if char == ' ' or char == '-' or i == #text then
 			if lineWidth + wordWidth > self.maxWidth then
 				formatted = formatted .. '\n'
 				lineWidth = 0
@@ -61,13 +97,16 @@ function textbox:setText(text)
 	self.timer = 0
 	self.index = 0
 	self.lines = 1
+	self.stopped = false
 end
 
 function textbox:skip(dt)
 	if self.timer > 0 then
 		self.timer = self.timer - dt
 	elseif self.index < self.text:len() then
-		self.sound:play()
+		if self.textSound then
+			self.textSound:play()
+		end
 		while self.timer <= 0 do
 			self.index = self.index+1
 
@@ -87,25 +126,37 @@ function textbox:skip(dt)
 				end
 			end
 		end
-	else
-		if self.index == self.text:len() then
-			self.callback()
-		end
+	elseif not self.stopped then
+		self.stopped = true
 	end
 end
 
 function textbox:update(dt)
+	if not self.visible then return end
 	self:skip(dt)
+	function love.keypressed(key, isrepeat)
+		if not self.visible then return end
+		if key == 'a' then
+			self:skip(self.beatSpeed*self.text:len())
+			if self.stopped then
+				if self.endSound then
+					self.endSound:play()
+				end
+				self:nextPage()
+			end
+		end
+	end
 end
 
 function textbox:draw()
-	love.graphics.setColor(100, 100, 100)
-	love.graphics.rectangle('fill', 100 - self.marginX, 100 - self.marginY, self.maxWidth + self.marginX*2, self.maxLines * self.font:getHeight() + self.marginY*2)
+	if not self.visible then return end
+	love.graphics.setColor(70, 70, 70)
+	love.graphics.rectangle('fill', self.x, self.y, self.maxWidth + self.paddingX*2, self.maxLines * self.font:getHeight() + self.paddingY*2)
 	love.graphics.setColor(255, 255, 255)
 
 	local oldFont = love.graphics.getFont()
 	love.graphics.setFont(self.font)
-	love.graphics.print(self.displayText, 100, 100)
+	love.graphics.print(self.displayText, self.x+self.paddingX, self.y+self.paddingY)
 	love.graphics.setFont(oldFont)
 end
 
